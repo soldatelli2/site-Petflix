44f22dfb2a15deba057f2e4e5f9a994213c77f16
MATHEUS SANTOS SOLDATELLI <matheus_soldatelli@estudante.sesisenai.org.br>
2024-08-05 21:58:52 -0300

    feat: Adiciona funcionalidade da tela splashscreen

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.getElementById('splash-screen').style.display = 'none';
+        document.getElementById('main-content').style.display = 'block';
+    }, 2500);
+});
